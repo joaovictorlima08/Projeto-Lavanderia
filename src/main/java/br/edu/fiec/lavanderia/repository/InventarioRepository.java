a560327f27609586ecd4645469a1e947ecfdfd6d
João Victor Lima <joao46123@fiec.edu.br>
2026-06-16 07:19:59 -0300

    Finalizado!!

--- src/main/java/br/edu/fiec/lavanderia/repository/InventarioRepository.java
+++ src/main/java/br/edu/fiec/lavanderia/repository/InventarioRepository.java
@@ -4,7 +4,15 @@ import br.edu.fiec.lavanderia.model.entity.InventarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InventarioRepository extends JpaRepository<InventarioEntity, Long> {
+
+    List<InventarioEntity> findByMarca(String marca); // Busca por marca
+
+    List<InventarioEntity> findByModelo(String modelo); // Busca por modelo
+
+    List<InventarioEntity> findByLitragem(Integer litragem); // Busca por litragem
 
 }
--- src/main/java/br/edu/fiec/lavanderia/repository/ReservaRepository.java
+++ src/main/java/br/edu/fiec/lavanderia/repository/ReservaRepository.java
@@ -4,6 +4,17 @@ import br.edu.fiec.lavanderia.model.entity.ReservaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReservaRepository extends JpaRepository<ReservaEntity, Long> {
+
+    List<ReservaEntity> findByCliente(String cliente); // Busca reservas pelo nome do cliente
+
+    List<ReservaEntity> findByTelefone(String telefone); // Busca reservas pelo telefone
+
+    List<ReservaEntity> findByMaquinaId(Long maquinaId); // Busca reservas pelo ID da máquina
+
+    List<ReservaEntity> findByDataReserva(String dataReserva); // Busca reservas pela data da reserva
+
 }
